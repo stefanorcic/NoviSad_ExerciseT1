0db2b9d1fb15e14672d5740004f879c907890420
somename <someemail>
2017-05-11 19:38:53 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -25,7 +25,7 @@ public class BowlingGame {
 	
 	// Returns the game score
 	public int score(){
-		//to be implemented: should return game score 
+		//to be implemented: should return game score
 		return 0;
 	}
 }
