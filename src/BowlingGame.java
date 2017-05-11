aacd6129f3db28ded0c647b7f91ab3447138879f
somename <someemail>
2017-05-11 19:50:20 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -9,6 +9,7 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
+	int score = 0;
 	
 	public BowlingGame(){}
 	
@@ -26,6 +27,9 @@ public class BowlingGame {
 	// Returns the game score
 	public int score(){
 		//to be implemented: should return game score
+		for (Frame frame : frames) {
+			
+		}
 		return 0;
 	}
 }
--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 public class TestBowling {
 
 	@Test
-	public void test() {
+	public void testFrame1()  throws BowlingException  {
 		Frame fr = new Frame(1, 4);
 		int expected = 5;
 		assertEquals(expected, fr.score());
 	}
+	
+	@Test
+	public void testFrame2()  throws BowlingException  {
+		Frame fr = new Frame(4, 5);
+		int expected = 9;
+		assertEquals(expected, fr.score());
+	}
+	
 
 }
