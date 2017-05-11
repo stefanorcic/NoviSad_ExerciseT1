3f590b46a6443f79af9e4efd089cfeca384d96b0
somename <someemail>
2017-05-11 19:36:23 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -15,6 +15,7 @@ public class BowlingGame {
 	// adds a frame to the game
 	public void addFrame(Frame frame){
 		//to be implemented
+		frames.add(frame);
 	}
 	
 	// Sets the bonus throws at the end of the game
